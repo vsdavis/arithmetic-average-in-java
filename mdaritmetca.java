27457268fb72f1ddc0adca86dac39dce497c6c1a
DavisWeb <111157801+vsdavis@users.noreply.github.com>
2023-07-24 18:32:27 -0300

    Add files via upload

---
+++ mdaritmetca.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class mdaritmetca {
+
+    public static void main (String[] args) {
+        Scanner sc = new Scanner (System.in);
+        double x, y, media;
+
+        System.out.print("Digite o primeiro numero: ");
+        x = sc.nextDouble();
+        System.out.print("Digite o segundo numero: ");
+        y = sc.nextDouble();
+        media = (x + y) / 2.0;
+        System.out.println("Media = " + media);
+        sc.close();
+    }
+
+}
